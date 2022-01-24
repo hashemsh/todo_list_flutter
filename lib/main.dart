0d290f1ce49df96a4568c603ea4cbaf435a158f8
hashem sheikhypour <sh.sheikhypour@gmail.com>
2022-01-24 18:30:36 +0330

    make HomeScreen and addNewTask

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_list_flutter/data.dart';
 
+const taskBoxName = 'task';
 void main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  Hive.registerAdapter(PriorityAdapter());
+  await Hive.openBox<Task>(taskBoxName);
   runApp(const MyApp());
 }
 
@@ -15,19 +20,75 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        decoration: BoxDecoration(color: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final box = Hive.box<Task>(taskBoxName);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ToDo List'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => EditTaskScreen()));
+          },
+          label: const Text('add new task')),
+      body: ListView.builder(
+        itemCount: box.values.length,
+        itemBuilder: (context, index) {
+          final Task task = box.values.toList()[index];
+          return Text(
+            task.name,
+            style: TextStyle(fontSize: 24),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class EditTaskScreen extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
+
+  EditTaskScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('edit task'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            final task = Task();
+            task.name = _controller.text;
+            task.priority = Priority.low;
+            if (task.isInBox) {
+              task.save();
+            } else {
+              final Box<Task> box = Hive.box(taskBoxName);
+              box.add(task);
+            }
+            Navigator.of(context).pop();
+          },
+          label: const Text('save change')),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration:
+                const InputDecoration(label: Text('add a task for today ... ')),
+          )
+        ],
       ),
     );
   }
